c494de8cda1d0c9cbff8cd0deae38bd7c7563dce
George Alehandro Saad <74742818+GeorgeAlehandro@users.noreply.github.com>
2022-02-28 00:42:06 +0100

    stable but heavier

--- Huffman.py
+++ Huffman.py
@@ -47,6 +47,7 @@ class Huffman:
         self.reconstructed_binary = None
         self.decrypted_chain = None
         self.dict_compressed = None
+
     def calcul_freq(self):
         '''
         Calculer la frequence de chaque lettre
@@ -136,6 +137,7 @@ class Huffman:
         compressed = self.compressed  # String of 0s and ones
         # Binary sequence will be divided into pieces of 8
         partition = [compressed[i:i + 8] for i in range(0, len(compressed), 8)]
+        print(partition)
         all_encoded = ''
         for i in range(len(partition)):
             all_encoded += chr(int(partition[i], 2))
@@ -198,23 +200,35 @@ class Huffman:
         return decrypted_chain
 
     def save_transformation(self, file):
-        file = open(file, 'w')
-        file.write(self.all_encoded)
-        file.write(str(self.dict_compressed))
+        file = open(file, 'wb')
+      #  file.write(self.all_encoded)
+        print('The whole sequence written is ')
+        print(len(self.all_encoded))
+        print(len(self.dict_compressed))
+        print(self.all_encoded+self.dict_compressed)
+       # test_zip = zlib.compress(self.all_encoded+self.dict_compressed)
+        # file.write(self.all_encoded+self.dict_compressed)
+        pickle.dump(self.all_encoded+self.dict_compressed, file)
+        print('Whole len should be written: ' +
+              str((len(self.all_encoded)+len(self.dict_compressed))))
         file.close()
 
     def load_transformation(self, file):
-        file = open(file, 'r')
-        sequence = str(file.readline())
-        begin_dict = sequence.index('{')
+        file = open(file, 'rb')
+       # sequence = file.read()
+        sequence = pickle.load(file)
+        begin_dict = sequence.index('□')
+        #begin_dict = sequence.index('{')
+        print('Whole len of the loaded sequence: ' + str(len(sequence)))
+        print(sequence)
         str_rep_of_dic = sequence[begin_dict:len(sequence)]
         self.dict_compressed = str_rep_of_dic
+        print("Check here", self.dict_compressed)
         self.decompressing_dict()
         print(type(self.binary_tree_value))
         self.all_encoded = sequence[0:begin_dict]
-        print('dictionnaire',self.binary_tree_value)
-        print('here',self.all_encoded)
-        print(file)
+        print('dictionnaire', self.binary_tree_value)
+        print('here', self.all_encoded)
 
     def dict_values(self):
         compress = ''
@@ -232,6 +246,7 @@ class Huffman:
     def all_decompressing(self):
         self.binary_decoding()
         self.binary_decompression()
+
     def compressing_dict(self):
         '''
         Used to compress the dictionnary (self.binary_tree_value)
@@ -241,51 +256,64 @@ class Huffman:
         values_compressed = ''
         for value in self.binary_tree_value.values():
             protection = '1' + value
-            #print(value)
-            print(protection)
-            #int_from_binary = str(int(protection,2))
+            # print(value)
+            # int_from_binary = str(int(protection,2))
             values_compressed += chr(int(protection, 2))
-        print(values_compressed)
-        print(len(values_compressed))
-        values_compressed = '{' + values_compressed +'}'
-        print(len(values_compressed))
-        check = ['A','T','C','G','N']
+      #  values_compressed = '□'+values_compressed + '□'
+        values_compressed = '□'+values_compressed + '□'
+        check = ['A', 'T', 'C', 'G', 'N', '$']
         for nuc in check:
             if nuc not in self.binary_tree_value.keys():
                 values_compressed += nuc
         print(values_compressed)
+        print(len(values_compressed))
         self.dict_compressed = values_compressed
+        print(len(self.dict_compressed))
         return values_compressed
 
     def decompressing_dict(self):
+        lst = []
+        print('Length of seld.dict_compressed', len(self.dict_compressed))
+        for pos, char in enumerate(self.dict_compressed):
+            if char == '□':
+                lst.append(pos)
+                print('FOUND')
+                print(pos)
+        print('This is list', lst)
         to_map = []
-        begin_number = self.dict_compressed.index('{')+1
-        end_number = self.dict_compressed.index('}')
-        print(begin_number, end_number)
-        numbers = self.dict_compressed[begin_number:end_number] #only takes the numbers
+        # begin_number = self.dict_compressed.index('{')+1
+        # end_number = self.dict_compressed.index('}')
+       # print(begin_number, end_number)
+        # only takes the numbers
+        numbers = self.dict_compressed[lst[0]+1:lst[1]]
         print(numbers)
-        #print(numbers)
-        #print(len(numbers))
+        # print(numbers)
+        # print(len(numbers))
         print('here')
         print(to_map)
         for number in numbers:
-            back_to_old = bin(ord(number))[3:]#Removes 0b1
+            back_to_old = bin(ord(number))[3:]  # Removes 0b1
          #   print((back_to_old))
             to_map.append(back_to_old)
             print(back_to_old)
         print(to_map)
-        new = {'A': None, 'C': None, 'G': None, 'N': None,'T': None}
-        letters = [word for word in self.dict_compressed if word.isalpha()]
+        new = {'$': None, 'A': None, 'C': None, 'G': None,
+               'N': None, 'T': None}
+        letters = [word for word in self.dict_compressed if word.isalpha()
+                   or word == '$']
         for letter in letters:
             del new[letter]
+        print(new)
         keys_of_dict = list(new)
-        #print(keys_of_dict)
-        #print(len(to_map))
+        # print(keys_of_dict)
+        print(len(to_map))
+        print(len(new))
         for i in range(len(new)):
             new[keys_of_dict[i]] = to_map[i]
         print(self.binary_tree_value)
         self.binary_tree_value = new
-        print(new) 
+        print(new)
+
 
 def dict2File(data, filename):
     bytes = io.BytesIO()
@@ -305,18 +333,32 @@ def file2Dict(filename):
     return pickle.loads(bytes)
 
 
-
-test_all_new = Huffman(
-    'ATAGACAGATACTACGATCGATCGATCGATCATCAGCTGAGCTACGGCGCATGCATG')
+test_all_new = Huffman('ATGGCGGC$GGTCACGTATTAAACAA')
+# ATGGCGGC$GGTCACGTATTAAACAA test with this
+test_all_new.all_compressing()
 test_all_new.compressing_dict()
-test_all_new.decompressing_dict()
+print('here test_all.all_encoded '+str(len(test_all_new.all_encoded)))
+# print(type(test_all_new.all_encoded))
+# print(test_all_new.all_encoded)
+# print(type(test_all_new.dict_compressed))
+# print(test_all_new.dict_compressed)
+# print(test_all_new.compressed)
+# print('here test_all.compressed_cict '+str(len(test_all_new.dict_compressed)))
+# print('herllo')'
+print(test_all_new.binary_tree_value)
+# test_all_new.decompressing_dict()
 test_all_new.save_transformation('test_mix.txt')
+# print(test_all_new.compressed)
+# print(test_all_new.binary_tree_value)
 last_test = Huffman()
 last_test.load_transformation('test_mix.txt')
-
+last_test.all_decompressing()
+print('Same dictionnary codage? ' +
+      str(str(last_test.dict_compressed == test_all_new.dict_compressed)))
+print('Same dictionarries?' +
+      str(last_test.binary_tree_value == test_all_new.binary_tree_value))
+# print(last_test.all_encoded)
 # test_all_new.save_transformation('test_dico_big_combo.txt')
 # test_load = Huffman()
 # test_load.load_transformation('test_dico_big_combo.txt')
 # test_load.all_decompressing()
-
-## almost 90% compression gj
