21b5f9564abcb515435343c9c1904c65b0c248da
George Alehandro Saad <74742818+GeorgeAlehandro@users.noreply.github.com>
2022-03-13 23:42:01 +0100

    Update Huffman.py

--- Huffman.py
+++ Huffman.py
@@ -11,56 +11,85 @@ import pickle
 import zlib
 import ast
 import codecs
+import networkx as nx
+import matplotlib.pyplot as plt
+from collections import defaultdict
+import copy
 
 
 class Node:
+    '''
+    Nodes class used to create the tree of the Huffman's compression
+    '''
+
     def __init__(self, freq, symbol, left=None, right=None):
-
+        '''
+        Constructor of the Nodes class
+        '''
+        # For each node there is a calculated frequence
         self.freq = freq
-
+        # For each node there is a symbol
         self.symbol = symbol
-
+        # For each node if there it has another node on its left
         self.left = left
-
+        # For each node if there it has another node on its right
         self.right = right
-
+        # Calculations for each path to the node, being a sequence of 0s and 1s
         self.branch = ''
 
     def __str__(self):
+        '''
+        Modifiying the print statement for each node object
+        '''
+        # The print statement will return the frequence and symbol of each node
         info = str(self.freq) + self.symbol
         return info
-
+    # To specify how the object will be presented textually when printing
     __repr__ = __str__
 
 
 class Huffman:
-    def __init__(self, motif=None):
-        if motif is not None:
-            self.motif = motif
+    '''
+    Huffman class for all the Huffman's operations including compression and
+    decompression
+    '''
+
+    def __init__(self, entry=None):
+        '''
+        Constructor of the class
+        '''
+        # Sequence is the combination that will be compressed
+        self.sequence = None
+        if entry is not None:
+            # Verification of entry and unifying the syntax eliminating spaces
+            if all(letter in ['A', 'T', 'C', 'G', 'N', '\n', '$'] for letter in entry.upper()):
+                self.sequence = entry.lstrip().rstrip().replace('\n', "").upper()
+            else:
+                self.coding_partition(entry)
         else:
-            self.motif = None
-        self.dico = None
-        self.nodes = None
-        self.tree = None
-        self.binary_tree_value = None
-        self.compressed = None
-        self.all_encoded = None
-        self.reconstructed_binary = None
-        self.decrypted_chain = None
-        self.dict_compressed = None
+            self.sequence = None
+        # self.dico = None
+        # self.nodes = None
+        # self.tree = None
+        # self.binary_tree_value = None
+        # self.compressed = None
+        # self.all_encoded = None
+        # self.reconstructed_binary = None
+        # self.decrypted_chain = None
+        # self.dict_compressed = None
 
     def calcul_freq(self):
         '''
         Calculer la frequence de chaque lettre
         '''
         dico = {}
-        for a in self.motif:
+        for a in self.sequence:
             if a not in dico:
                 dico[a] = 1
             else:
                 dico[a] += 1
         self.dico = dico
-        return dico
+        return 'Has the following frequences ' + str(dico)
 
     def create_nodes(self):
         '''
@@ -73,7 +102,7 @@ class Huffman:
             new_node = Node(freq, symb)
             nodes.append(new_node)
         self.nodes = nodes
-        return nodes
+        return 'Which will constitute theses nodes ' + str(nodes)
 
     def tree_parser(self):
         '''
@@ -99,7 +128,7 @@ class Huffman:
             # insert newNode inside (sum of the smallest two nodes)
             tree.append(newNode)
         self.tree = tree
-        return tree  # LIST OF A NODE
+        return 'Overview of the tree ' + str(tree)  # LIST OF A NODE
 
     def recursive_parcours(self, node=None, val='', binary_tree_value={}):
         '''
@@ -118,18 +147,19 @@ class Huffman:
             # print(f"{node.symbol} -> {newVal}")
             binary_tree_value[node.symbol] = newVal
         self.binary_tree_value = binary_tree_value
-        return(binary_tree_value)
+        return 'The following resulting dictionnary ' + str(binary_tree_value)
 
     def binary_transformation(self):
         '''
-        Transforms the motif into a binary number based on the tree's values
+        Transforms the sequence into a binary number based on the tree's values
         '''
         binary_tree_value = self.binary_tree_value
         compressed = ''
-        for nuc in self.motif:
+        for nuc in self.sequence:
             compressed += binary_tree_value[nuc]
         self.compressed = compressed
-        return compressed
+       # return 'The nucleotide sequence is then turned into :' + str(compressed)
+        return self.sequence + '---->' + str(compressed)
 
     def binary_coding(self):
         '''
@@ -138,7 +168,7 @@ class Huffman:
         compressed = self.compressed  # String of 0s and ones
         # Binary sequence will be divided into pieces of 8
         partition = [compressed[i:i + 8] for i in range(0, len(compressed), 8)]
-        print(partition)
+       # print(partition)
         all_encoded = ''
         for i in range(len(partition)):
             all_encoded += chr(int(partition[i], 2))
@@ -148,9 +178,9 @@ class Huffman:
 
         all_encoded += str(len(partition[-1]))
 
-        print(self.motif + ' has the code ' + all_encoded)
+        # print(self.sequence + ' has the code ' + all_encoded)
         self.all_encoded = all_encoded
-        return (all_encoded)
+        return self.sequence + '---->' + str(compressed) + '---->'+str(all_encoded)
 
     def binary_decoding(self):
         '''
@@ -171,10 +201,10 @@ class Huffman:
             else:
                 reconstructed_binary.append(
                     bin(ord(all_encoded[i]))[2:].zfill(8))
-#        print(self.motif + ' becomes ' + str(reconstructed_binary))
+#        print(self.sequence + ' becomes ' + str(reconstructed_binary))
         self.reconstructed_binary = reconstructed_binary
         self.compressed = ''.join(reconstructed_binary)
-        return reconstructed_binary
+        return 'The binary reconstruction of the sequence is '+str(reconstructed_binary)
 
     def binary_decompression(self):
         '''
@@ -196,43 +226,44 @@ class Huffman:
             else:
                 # Takes the unmatched part, adds it to the next iteration
                 residual = i
-        print(str(compressed)+' becomes ' + ''.join(key_list))
         decrypted_chain = ''.join(key_list)
+        # print(str(compressed)+' becomes below')
+        # print(decrypted_chain)
         self.decrypted_chain = decrypted_chain
-        return decrypted_chain
+        self.sequence = decrypted_chain
+        return str(compressed)+' becomes ' + str(decrypted_chain)
 
     def save_transformation(self, file):
         file = open(file, 'w')
         file.write(self.all_encoded)
-        print('The whole sequence written is ')
-        print(len(self.all_encoded))
-        print(len(self.dict_compressed))
-        print(self.all_encoded+self.dict_compressed)
+        # print('The whole sequence written is ')
+        # print(len(self.all_encoded))
+        # print(len(self.dict_compressed))
+        # print(self.all_encoded+self.dict_compressed)
         file.write(self.dict_compressed)
-        print('Whole len should be written: ' +
-              str((len(self.all_encoded)+len(self.dict_compressed))))
+        # print('Whole len should be written: ' +
+        #       str((len(self.all_encoded)+len(self.dict_compressed))))
         file.close()
+
+    def coding_partition(self, entry):
+        begin_dict = entry.index('□')
+        str_rep_of_dic = entry[begin_dict:len(entry)]
+        self.dict_compressed = str_rep_of_dic
+        # print(self.dict_compressed)
+        self.decompressing_dict()
+        # print('coding_partition')
+        # print(entry)
+        self.all_encoded = entry[0:begin_dict]
 
     def load_transformation(self, file):
         file = codecs.open(file, 'r', encoding='utf-8')
         sequence = file.read()
-        begin_dict = sequence.index('{')
-        #begin_dict = sequence.index('{')
-        print('Whole len of the loaded sequence: ' + str(len(sequence)))
-        print(sequence)
-        str_rep_of_dic = sequence[begin_dict:len(sequence)]
-        self.dict_compressed = str_rep_of_dic
-        print("Check here", self.dict_compressed)
-        self.decompressing_dict()
-        print(type(self.binary_tree_value))
-        self.all_encoded = sequence[0:begin_dict]
-        print('dictionnaire', self.binary_tree_value)
-        print('here', self.all_encoded)
+        self.coding_partition(sequence)
 
-    def dict_values(self):
-        compress = ''
-        for value in self.binary_tree_value.values():
-            print(value)
+    # def dict_values(self):
+    #     compress = ''
+    #     for value in self.binary_tree_value.values():
+    #         print(value)
 
     def all_compressing(self):
         self.calcul_freq()
@@ -249,75 +280,118 @@ class Huffman:
     def compressing_dict(self):
         '''
         Used to compress the dictionnary (self.binary_tree_value)
-        { + compressed values each in one character + } + missing_letters
+        □ + compressed values each in one character + □ + missing_letters
         '''
         self.binary_tree_value = dict(sorted(self.binary_tree_value.items()))
         values_compressed = ''
         for value in self.binary_tree_value.values():
             protection = '1' + value
             # print(value)
-            print(protection)
             # int_from_binary = str(int(protection,2))
             values_compressed += chr(int(protection, 2))
-        print(values_compressed)
-        print(len(values_compressed))
       #  values_compressed = '□'+values_compressed + '□'
-        values_compressed = '{'+values_compressed + '}'
-        print(values_compressed)
-        print(len(values_compressed))
+        values_compressed = '□'+values_compressed + '□'
         check = ['A', 'T', 'C', 'G', 'N', '$']
         for nuc in check:
             if nuc not in self.binary_tree_value.keys():
                 values_compressed += nuc
-        print(values_compressed)
-        print(len(values_compressed))
         self.dict_compressed = values_compressed
-        print(len(self.dict_compressed))
-        return values_compressed
+        self.full_sequence_compressed = self.all_encoded + self.dict_compressed
+        return 'The dictionnary is encoded as' + '"'+str(values_compressed)+'"'
+
+    def pedagogic_compressing(self):
+        yield self.sequence
+        yield self.calcul_freq()
+        yield self.create_nodes()
+        self.tree_parser()
+      #  yield self.draw(self.tree[0], 0)
+        yield self.recursive_parcours()
+        yield self.sequence
+        yield self.binary_transformation()
+        yield self.binary_coding()
+        yield self.compressing_dict()
+        yield 'Full encoding of the sequence: ' + '"'+self.all_encoded + self.dict_compressed+'"'
+
+    def pedagogic_decompressing(self):
+        yield self.binary_decoding()
+        yield self.binary_decompression()
+        yield self.decompressing_dict()
+        # yield 'Full encoding of the sequence: ' + self.all_encoded + self.dict_compressed
 
     def decompressing_dict(self):
         lst = []
-        print('Length of seld.dict_compressed', len(self.dict_compressed))
-        # for pos, char in enumerate(self.dict_compressed):
-        #     if char == '□':
-        #         lst.append(pos)
-        #         print('FOUND')
-        #         print(pos)
-        print('This is list', lst)
+        for pos, char in enumerate(self.dict_compressed):
+            if char == '□':
+                lst.append(pos)
         to_map = []
-        begin_number = self.dict_compressed.index('{')+1
-        end_number = self.dict_compressed.index('}')
-       # print(begin_number, end_number)
-        # only takes the numbers
+        begin_number = lst[0]+1
+        end_number = lst[1]
         numbers = self.dict_compressed[begin_number:end_number]
-        #numbers = self.dict_compressed[lst[0]+1:lst[1]]
-        print(numbers)
-        # print(numbers)
-        # print(len(numbers))
-        print('here')
-        print(to_map)
         for number in numbers:
             back_to_old = bin(ord(number))[3:]  # Removes 0b1
          #   print((back_to_old))
             to_map.append(back_to_old)
-            print(back_to_old)
-        print(to_map)
         new = {'$': None, 'A': None, 'C': None, 'G': None,
                'N': None, 'T': None}
         letters = [word for word in self.dict_compressed if word.isalpha()
                    or word == '$']
         for letter in letters:
             del new[letter]
-        print(new)
         keys_of_dict = list(new)
         # print(keys_of_dict)
-        print(len(to_map))
-        print(len(new))
         for i in range(len(new)):
             new[keys_of_dict[i]] = to_map[i]
-        print(self.binary_tree_value)
         self.binary_tree_value = new
-        print(new)
+        return 'The reconstruction of the dictionary gives back ' + str(self.binary_tree_value)
+
+    def draw(self, node, data):   # Draw a node as root
+        saw = defaultdict(int)
+
+        def create_graph(G, node, p_name="initvalue", pos={}, x=0, y=0, layer=1):
+            if not node:
+                return
+            name = str(node.symbol)
+           # print("node.name:", name, "x,y:(", x, ",", y, ")l_layer:", layer)
+            saw[name] += 1
+            if name in saw.keys():
+                name += ' ' * saw[name]
+            if p_name != "initvalue":
+                G.add_edge(p_name, name)
+            pos[name] = (x, y)
+
+            l_x, l_y = x - 1 / (3 * layer), y - 1
+            # print("l_x, l_y:",l_x, l_y)
+            l_layer = layer + 1
+            # print("l_layer:",l_layer)
+            create_graph(G, node.left, name, x=l_x,
+                         y=l_y, pos=pos, layer=l_layer)
+
+            r_x, r_y = x + 1 / (3 * layer), y - 1
+            # print("r_x, r_y:", r_x, r_y)
+            r_layer = layer + 1
+            create_graph(G, node.right, name, x=r_x,
+                         y=r_y, pos=pos, layer=r_layer)
+            return (G, pos)
+
+        graph = nx.DiGraph()
+        graph.name
+        graph, pos = create_graph(graph, node)
+        # pos["     "] = (0, 0)
+        # print("pos:", pos)
+        # Scale can be adjusted appropriately according to the depth of the tree
+        fig, ax = plt.subplots(figsize=(8, 10))
+        color_map = []
+        # for j,node in enumerate(graph.nodes):
+        # for degree in graph.out_degree:
+        #     if int(degree[0]) in data and degree[1] == 0:
+        #         color_map.append('blue')
+        #     else:
+        #         color_map.append('green')
+        # nx.draw(G, node_color=color_map, with_labels=True)
+        nx.draw_networkx(graph, pos, ax=ax, node_size=1000,
+                         node_color=color_map)
+        plt.show()
+        return 'With this tree'
 
 
 def dict2File(data, filename):
@@ -336,23 +410,3 @@ def file2Dict(filename):
     # decompress this zbytes again
     bytes = zlib.decompress(zbytes)
     return pickle.loads(bytes)
-
-
-test_all_new = Huffman('ATGGCGGC$GGTCACGTATTAAACAA')
-# ATGGCGGC$GGTCACGTATTAAACAA test with this
-test_all_new.all_compressing()
-test_all_new.compressing_dict()
-print('here test_all.all_encoded '+str(len(test_all_new.all_encoded)))
-print(test_all_new.binary_tree_value)
-test_all_new.save_transformation('test_mix')
-last_test = Huffman()
-last_test.load_transformation('test_mix')
-last_test.all_decompressing()
-print(test_all_new.compressed)
-print(last_test.compressed)
-print('Same sequence codage? ' +
-      str(str(last_test.compressed == test_all_new.compressed)))
-print('Same dictionnary codage? ' +
-      str(str(last_test.dict_compressed == test_all_new.dict_compressed)))
-print('Same dictionarries?' +
-      str(last_test.binary_tree_value == test_all_new.binary_tree_value))
